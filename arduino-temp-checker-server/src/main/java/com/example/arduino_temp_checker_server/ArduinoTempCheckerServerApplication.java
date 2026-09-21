f38aec692e031888b1109b4835c49b19ea1a47c9
Jonatan Andersson <Jonatan.anderssons@hotmail.com>
2026-09-21 20:29:05 +0200

    initiate project

---
+++ arduino-temp-checker-server/src/main/java/com/example/arduino_temp_checker_server/ArduinoTempCheckerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.arduino_temp_checker_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoTempCheckerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArduinoTempCheckerServerApplication.class, args);
+	}
+
+}
---
+++ arduino-temp-checker-server/src/test/java/com/example/arduino_temp_checker_server/ArduinoTempCheckerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arduino_temp_checker_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArduinoTempCheckerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
