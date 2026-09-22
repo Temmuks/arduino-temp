0561cc4fc9061c40a13cb7255930d6cead58f59c
Jonatan Andersson <Jonatan.anderssons@hotmail.com>
2026-09-22 11:17:26 +0200

    Added support for tempSensor, and updated postFunction to send the real temperature Value

--- Arduino-code/Arduino-tempChecker/Arduino-tempChecker.ino
+++ Arduino-code/Arduino-tempChecker/Arduino-tempChecker.ino
@@ -2,16 +2,31 @@
 #include <Arduino_LED_Matrix.h>
 #include <ArduinoHttpClient.h>
 
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
 #include "frames.h"
 #include "secrets.h"
 
+//Wifi settings
 char ssid[] = SECRET_SSID;
 char password[] = SECRET_PASS;
+
+
+//API Settings
 char api[] = SECRET_API;
 int port = 8080;
+char endpoint[] = "/test";
 
 WiFiClient wifi;
 HttpClient client = HttpClient(wifi, api, port);
+
+//Sensor Settings
+  #define ONE_WIRE_BUS 2
+  OneWire oneWire(ONE_WIRE_BUS);
+  DallasTemperature sensors(&oneWire);
+  int tempC;
+  
 
 ArduinoLEDMatrix matrix;
 
@@ -20,6 +35,7 @@ void setup() {
 
   Serial.begin(9600);
 
+  sensors.begin();
   matrix.begin();
 
   Serial.println("Connecting to Wifi...");
@@ -47,18 +63,26 @@ void loop() {
     delay(500);
   }
 
-  postTemp(50, client);
+  tempC = sensorCheck();
 
-  delay(2000);
+  postTemp(tempC, client);
+
+  delay(5000);
 }
 
+  int sensorCheck(){
+    sensors.requestTemperatures();
+
+   return sensors.getTempCByIndex(0);
+
+  }
 
 void postTemp(int newTemp, HttpClient client) {
   String postData = String("{\"temp\":") + newTemp + "}";
   Serial.println("Sending postData...");
 
   client.beginRequest();
-  client.post("/test");
+  client.post(endpoint);
 
   client.sendHeader("Content-Type", "application/json");
   client.sendHeader("Content-Length", postData.length());
